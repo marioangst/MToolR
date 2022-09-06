4f954246c575fb16f287f59af964748b9fddfd59
Mario Angst <mario.angst@gmail.com>
2022-09-06 17:57:38 +0200

    startup message

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onLoad <- function(libname, pkgname){
+  cat("MToolR is a companion package for the Mental Model Mapping Tool M-Tool (https://www.m-tool.org/)
+
+      Wouter van Boxtel wrote the initial scripts that inspired the functionality of this package. Thanks Wouter ;)!
+
+    This package is under development. Use at your own risk. Help us by reporting bugs and create feature requests here: https://github.com/marioangst/MToolR/issues")
+}
